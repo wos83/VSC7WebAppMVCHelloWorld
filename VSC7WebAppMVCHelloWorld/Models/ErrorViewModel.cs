1135d602970432fafe4fccc514aef6d2408c3c5f
Willian Santos <m16@m16.com.br>
2023-04-09 23:16:35 -0300

    Alteração Página Index

--- VSC7WebAppMVCHelloWorld/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VSC7WebAppMVCHelloWorld.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
---
+++ VSC7WebAppMVCHelloWorld/Models/HomeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VSC7WebAppMVCHelloWorld.Models
+{
+	public class HomeModel
+	{
+		public string? myName  { get; set; }
+        public string? myMail  { get; set; }
+        public string? myPhone { get; set; }
+    }
+
+    public static class HomeModelRepo
+    {
+        private static List<HomeModel> homeVisits = new List<HomeModel>();
+        public static IEnumerable<HomeModel> HomeModels { get { return homeVisits; } }
+        public static void AddHomeVisits(HomeModel homeVisit)
+        {
+            homeVisits.Add(homeVisit);
+        }
+    }
+}
